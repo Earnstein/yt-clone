b0f3f2969619f8993d7d148b45509c5eb193f393
earnstein <bdamilola00@gmail.com>
2025-02-11 23:09:12 +0100

    chore: Remove unused home page client and protected route components

--- src/app/(home)/client.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-import { trpc } from "@/trpc/client";
-export default function HomeClient() {
-  const [data] = trpc.hello.useSuspenseQuery({ text: "Bakare Damilola" });
-  return <div>{data.greeting}</div>;
-}
