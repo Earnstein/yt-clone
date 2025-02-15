b4ff7c7b5337c942561bb2a0500f0f347d4d02db
earnstein <bdamilola00@gmail.com>
2025-02-15 21:01:43 +0100

    feat: Add upload modal with tRPC mutation for video creation

--- src/modules/studio/ui/components/studio-upload-modal.tsx
+++ src/modules/studio/ui/components/studio-upload-modal.tsx
@@ -1,11 +1,43 @@
+"use client";
+import { ResponsiveModal } from "@/components/responsive-modal";
 import { Button } from "@/components/ui/button";
+import { trpc } from "@/trpc/client";
 import { PlusIcon } from "lucide-react";
+import { toast } from "sonner";
 
 export const StudioUploadModal = () => {
+  const utils = trpc.useUtils();
+  const mutation = trpc.videos.createVideo.useMutation({
+    onSuccess: () => {
+      toast.success("Video created successfully");
+
+      utils.studio.getAllVideos.invalidate();
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
   return (
-    <Button variant="secondary">
-      <PlusIcon className="size-4" />
-      Create
-    </Button>
+    <>
+      <ResponsiveModal
+        open={!!mutation.data}
+        title="Upload Video"
+        description="Upload a video to your channel"
+        onOpenChange={() => {
+          mutation.reset();
+        }}
+      >
+        <div>...</div>
+      </ResponsiveModal>
+      <Button
+        variant="secondary"
+        onClick={() => mutation.mutate()}
+        disabled={mutation.isPending}
+        isLoading={mutation.isPending}
+      >
+        {!mutation.isPending && <PlusIcon className="size-4" />}
+        {mutation.isPending ? "Creating..." : "Create"}
+      </Button>
+    </>
   );
 };
