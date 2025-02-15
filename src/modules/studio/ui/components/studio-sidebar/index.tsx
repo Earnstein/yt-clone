93e5ee232131146a511d5a6c15231164691a685e
earnstein <bdamilola00@gmail.com>
2025-02-15 15:17:04 +0100

    feat: Add StudioSidebar component with content and exit navigation

---
+++ src/modules/studio/ui/components/studio-sidebar/index.tsx
@@ -0,0 +1,61 @@
+"use client";
+import { Separator } from "@/components/ui/separator";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { LogOutIcon, VideoIcon } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { StudioSidebarHeader } from "./studio-sidebar-header";
+export const StudioSidebar = () => {
+  const pathname = usePathname();
+  return (
+    <Sidebar className="z-40 pt-16 border-r" collapsible="icon">
+      <SidebarHeader className="flex items-center justify-center pb-4">
+        <StudioSidebarHeader />
+      </SidebarHeader>
+
+      <Separator />
+      <SidebarContent className="bg-background">
+        <SidebarGroup>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton
+                tooltip="Content"
+                asChild
+                isActive={pathname === "/studio"}
+              >
+                <Link href="/studio">
+                  <VideoIcon className="size-5" />
+                  <span className="text-sm">Content</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarGroup>
+      </SidebarContent>
+
+      <SidebarFooter>
+        <SidebarGroup>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton tooltip={"Exit Studio"} asChild>
+                <Link href="/">
+                  <LogOutIcon className="size-5" />
+                  <span className="text-sm"> Exit Studio</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarGroup>
+      </SidebarFooter>
+    </Sidebar>
+  );
+};
