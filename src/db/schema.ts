637261a9bb0a2b60e0d4b4431eed603b088fc43e
earnstein <bdamilola00@gmail.com>
2025-02-15 21:00:49 +0100

    feat: Add video, user, and category relations in Drizzle schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,10 +1,5 @@
-import {
-  pgTable,
-  text,
-  timestamp,
-  uniqueIndex,
-  uuid,
-} from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+import { pgTable, text, timestamp, uniqueIndex } from "drizzle-orm/pg-core";
 
 // TODO: TO remove clerkId and save it under id
 export const users = pgTable("users", {
@@ -21,7 +16,7 @@ export const users = pgTable("users", {
 export const categories = pgTable(
   "categories",
   {
-    id: uuid("id").primaryKey().defaultRandom(),
+    id: text("id").primaryKey(),
     name: text("name").notNull().unique(),
     description: text("description"),
     createdAt: timestamp("created_at").notNull().defaultNow(),
@@ -29,3 +24,46 @@ export const categories = pgTable(
   },
   (t) => [uniqueIndex("name_idx").on(t.name)]
 );
+
+export const videos = pgTable("videos", {
+  id: text("id").primaryKey(),
+  title: text("title").notNull(),
+  description: text("description"),
+  userId: text("user_id")
+    .references(() => users.id, {
+      onDelete: "cascade",
+    })
+    .notNull(),
+  categoryId: text("category_id").references(() => categories.id, {
+    onDelete: "set null",
+  }),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at").notNull().defaultNow(),
+});
+
+/*
+Note: This is for application-level use only and doesn't affect database-level relations.
+It's useful for query like builders as in Prisma (e.g., video.findMany({ include: { user: true } }))
+or for databases that do not support relations like SQLs e.g planetscale.
+It is not necessary for Drizzle's select and join operations. It's included here for learning purposes.
+
+refer to https://orm.drizzle.team/docs/relations for more information
+*/
+export const videoRelations = relations(videos, ({ one }) => ({
+  user: one(users, {
+    fields: [videos.userId],
+    references: [users.id],
+  }),
+  category: one(categories, {
+    fields: [videos.categoryId],
+    references: [categories.id],
+  }),
+}));
+
+export const userRelations = relations(users, ({ many }) => ({
+  videos: many(videos),
+}));
+
+export const categoryRelations = relations(categories, ({ many }) => ({
+  videos: many(videos),
+}));
