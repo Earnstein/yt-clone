7564d743cbedf09603b04c6aac46c8aaf0c3a264
earnstein <bdamilola00@gmail.com>
2025-02-15 21:01:35 +0100

    feat: Add tRPC procedure for fetching user's videos with pagination

---
+++ src/modules/studio/server/procedures.ts
@@ -0,0 +1,61 @@
+import { db } from "@/db";
+import { videos } from "@/db/schema";
+import { DEFAULT_LIMIT } from "@/lib/constants";
+import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
+import { and, desc, eq, lt, or } from "drizzle-orm";
+import { z } from "zod";
+export const studioRouter = createTRPCRouter({
+  getAllVideos: protectedProcedure
+    .input(
+      z.object({
+        cursor: z
+          .object({
+            id: z.string(),
+            updatedAt: z.date(),
+          })
+          .nullish(),
+        limit: z.number().min(1).max(100).default(DEFAULT_LIMIT),
+      })
+    )
+    .query(async ({ input, ctx }) => {
+      const { cursor, limit } = input;
+      const { id: userId } = ctx.user;
+
+      const data = await db
+        .select()
+        .from(videos)
+        .where(
+          cursor
+            ? and(
+                eq(videos.userId, userId),
+                or(
+                  lt(videos.updatedAt, cursor.updatedAt),
+                  and(
+                    eq(videos.updatedAt, cursor.updatedAt),
+                    lt(videos.id, cursor.id)
+                  )
+                )
+              )
+            : eq(videos.userId, userId)
+        )
+        .orderBy(desc(videos.updatedAt), desc(videos.id))
+        // Add 1 to the limit to check if there is a next page
+        .limit(limit + 1);
+      const hasMore = data.length > limit;
+      //Remove the last item if there is more data
+      const items = hasMore ? data.slice(0, -1) : data;
+
+      //set the next cursor to the last item if there is more data
+      const lastItem = items[items.length - 1];
+      const nextCursor = hasMore
+        ? {
+            id: lastItem.id,
+            updatedAt: lastItem.updatedAt,
+          }
+        : null;
+      return {
+        items,
+        nextCursor,
+      };
+    }),
+});
