e2d6e0d742aa2ebbc5b733d16dfe28e3e69f8cfe
earnstein <bdamilola00@gmail.com>
2025-02-17 00:17:36 +0100

    feat: 🖼️ Add video thumbnail component with hover preview and duration

---
+++ src/modules/videos/ui/components/video-thumbnail.tsx
@@ -0,0 +1,50 @@
+import { formatDuration } from "@/lib/utils";
+import Image from "next/image";
+
+interface VideoThumbnailProps {
+  thumbnailUrl?: string | null;
+  previewUrl?: string | null;
+  title: string;
+  duration: number;
+}
+export const VideoThumbnail: React.FC<VideoThumbnailProps> = ({
+  thumbnailUrl,
+  previewUrl,
+  title,
+  duration,
+}) => {
+  return (
+    <div className="relative group">
+      {/*Thumbnail wrapper*/}
+      <div className="relative w-full overflow-hidden rounded-xl aspect-video">
+        <Image
+          unoptimized={!!thumbnailUrl}
+          loader={({ src }) => src || "/placeholder.svg"}
+          src={thumbnailUrl || "/placeholder.svg"}
+          alt={title}
+          fill
+          sizes="33vw"
+          className="object-cover size-full group-hover:opacity-0"
+          priority
+        />
+        <Image
+          unoptimized={!!previewUrl}
+          loader={({ src }) => src || thumbnailUrl || "/placeholder.svg"}
+          src={previewUrl || thumbnailUrl || "/placeholder.svg"}
+          alt={title}
+          fill
+          sizes="33vw"
+          className="object-cover size-full opacity-0 group-hover:opacity-100"
+          priority
+        />
+      </div>
+
+      {/*Video duration box*/}
+      <div className="absolute bottom-2 right-2 bg-black/80 text-white text-xs font-medium rounded">
+        {formatDuration(duration)}
+      </div>
+
+      {/* TODO: Add video duration box*/}
+    </div>
+  );
+};
