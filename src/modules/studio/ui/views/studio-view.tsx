8a1358d20a48b59dbce97dedf7cf1da26e6d719a
earnstein <bdamilola00@gmail.com>
2025-02-18 00:09:05 +0100

    feat: 📢 Add informative banner to StudioView for video status clarity

--- src/modules/studio/ui/views/studio-view.tsx
+++ src/modules/studio/ui/views/studio-view.tsx
@@ -1,15 +1,19 @@
-import { VideoSection } from "../sections/video-section";
+import { Banner } from "@/components/banner";
+import { VideoSection } from "../sections/videos-section";
 
 export const StudioView = () => {
   return (
-    <div className="space-y-6 pt-2.5">
-      <div className="px-4">
-        <h1 className="text-xl font-bold sm:text-2xl">Channel content</h1>
-        <p className="text-xs text-muted-foreground">
-          Manage your channel content and videos.
-        </p>
+    <>
+      <Banner title="Note: Some videos status may show as 'preparing' but are ready to play. Please check the video page to watch. 😎" />{" "}
+      <div className="space-y-6 pt-2.5">
+        <div className="px-4">
+          <h1 className="text-xl font-bold sm:text-2xl">Channel content</h1>
+          <p className="text-xs text-muted-foreground">
+            Manage your channel content and videos.
+          </p>
+        </div>
+        <VideoSection />
       </div>
-      <VideoSection />
-    </div>
+    </>
   );
 };
