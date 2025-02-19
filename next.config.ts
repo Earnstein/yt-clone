c5b74664c4c790ef50248531f48d53760f026f16
earnstein <bdamilola00@gmail.com>
2025-02-19 01:16:41 +0100

    feat: 🖼️ Configure Next.js image optimization for remote domains

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.mux.com",
+      },
+      {
+        protocol: "https",
+        hostname: "nlcbozdz32.ufs.sh",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
