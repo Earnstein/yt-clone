2155c0b9541b01a009a924bf3a4c17cdf43bc892
earnstein <bdamilola00@gmail.com>
2025-02-23 15:10:35 +0100

    refactor: Update video procedures import path

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,6 +1,6 @@
 import { categoriesRouter } from "@/modules/categrories/server/procedure";
 import { studioRouter } from "@/modules/studio/server/procedures";
-import { videosRouter } from "@/modules/videos/procedures";
+import { videosRouter } from "@/modules/videos/server/procedures";
 import { createTRPCRouter } from "../init";
 export const appRouter = createTRPCRouter({
   categories: categoriesRouter,
