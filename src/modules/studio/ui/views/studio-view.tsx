efecc58e742c33e73439f346d5a8a10e0625c0f1
earnstein <bdamilola00@gmail.com>
2025-02-23 18:18:04 +0100

    feat: ✌🏾Improve StudioView layout and Banner component with storage and auto-hide

--- src/modules/studio/ui/views/studio-view.tsx
+++ src/modules/studio/ui/views/studio-view.tsx
@@ -4,8 +4,12 @@ import { VideoSection } from "../sections/videos-section";
 export const StudioView = () => {
   return (
     <>
-      <Banner title="Note: Some videos status may show as 'preparing' but are ready to play. Please check the video page to watch. 😎" />{" "}
-      <div className="space-y-6 pt-2.5">
+      <Banner
+        title="Note: Some videos status may show as 'preparing' but are ready to play. Please check the video page to watch. 😎"
+        useLocalStorage
+        time={5000}
+      />{" "}
+      <div className="space-y-6 pt-2.5 px-4 max-w-[1700px] mx-auto">
         <div className="px-4">
           <h1 className="text-xl font-bold sm:text-2xl">Channel content</h1>
           <p className="text-xs text-muted-foreground">
