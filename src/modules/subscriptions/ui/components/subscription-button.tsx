ecdedd66ccc15039e54eaef46c910c9f07b0074c
earnstein <bdamilola00@gmail.com>
2025-02-23 18:18:46 +0100

    feat: Add SubscriptionButton component for user interaction

---
+++ src/modules/subscriptions/ui/components/subscription-button.tsx
@@ -0,0 +1,29 @@
+import { Button, ButtonProps } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+interface SubscriptionButtonProps {
+  onClick: ButtonProps["onClick"];
+  disabled: boolean;
+  isSubscribed: boolean;
+  className?: string;
+  size?: ButtonProps["size"];
+}
+
+export const SubscriptionButton: React.FC<SubscriptionButtonProps> = ({
+  isSubscribed,
+  disabled,
+  onClick,
+  className,
+  size,
+}) => {
+  return (
+    <Button
+      variant={isSubscribed ? "secondary" : "default"}
+      className={cn("rounded-full", className)}
+      size={size}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      {isSubscribed ? "Unsubscribe" : "Subscribe"}
+    </Button>
+  );
+};
