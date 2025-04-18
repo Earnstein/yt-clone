92bc5b91677232cce37ee4f65b22ab859e633e58
earnstein <bdamilola00@gmail.com>
2025-04-18 11:22:20 +0100

    feat: 🎉 Add subscriptions router to appRouter

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,5 +1,6 @@
 import { categoriesRouter } from "@/modules/categrories/server/procedure";
 import { studioRouter } from "@/modules/studio/server/procedures";
+import { subscriptionsRouter } from "@/modules/subscriptions/server/procedure";
 import { videoReactionsRouter } from "@/modules/video-reactions/server/procedure";
 import { videoViewsRouter } from "@/modules/video-views/server/procedure";
 import { videosRouter } from "@/modules/videos/server/procedures";
@@ -10,6 +11,7 @@ export const appRouter = createTRPCRouter({
   videos: videosRouter,
   videoViews: videoViewsRouter,
   videoReactions: videoReactionsRouter,
+  subscriptions: subscriptionsRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
