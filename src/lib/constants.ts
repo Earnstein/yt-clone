2bbfe776e22e9ad9320df3f0523efca5d67cab41
earnstein <bdamilola00@gmail.com>
2025-04-24 09:14:17 +0100

    feat: 🎉 Add comment and avatar placeholder constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,6 @@
 export const VIDEO_ID_PREFIX = "vd";
 export const CATEGORY_ID_PREFIX = "ct";
+export const COMMENT_ID_PREFIX = "cm";
 export const DEFAULT_LIMIT = 5;
 export const THUMBNAIL_PLACEHOLDER_URL = "/placeholder.svg";
+export const AVATAR_PLACEHOLDER_URL = "/avatar-placeholder.svg";
