0e41cd8f13e4ad70f3b89fc5813d5262c421c627
earnstein <bdamilola00@gmail.com>
2025-04-24 09:14:32 +0100

    feat: 🎉 Enhance CommentsSection with suspense and error handling

--- src/modules/videos/ui/sections/comments-sections.tsx
+++ src/modules/videos/ui/sections/comments-sections.tsx
@@ -1,3 +1,37 @@
-export const CommentsSection = () => {
-  return <div>CommentsSection</div>;
+"use client";
+
+import { CommentForm } from "@/modules/comments/ui/components/comment-form";
+import { trpc } from "@/trpc/client";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
+interface CommentsSectionProps {
+  videoId: string;
+}
+
+const CommentsSectionSuspense: React.FC<CommentsSectionProps> = ({
+  videoId,
+}) => {
+  const [comments] = trpc.comments.getMany.useSuspenseQuery({
+    videoId,
+  });
+
+  return (
+    <div className="mt-6">
+      <div className="flex flex-col gap-6">
+        <h2 className="text-lg font-medium">{comments.length} Comments</h2>
+        <CommentForm videoId={videoId} />
+      </div>
+    </div>
+  );
+};
+
+export const CommentsSection = ({ videoId }: CommentsSectionProps) => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ErrorBoundary fallback={<div>Error loading comments</div>}>
+        <CommentsSectionSuspense videoId={videoId} />
+      </ErrorBoundary>
+    </Suspense>
+  );
 };
