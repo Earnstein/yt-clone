52b39a3dc58c6c5c28c34f414d345607cb295656
earnstein <bdamilola00@gmail.com>
2025-04-30 09:36:30 +0100

    feat: 🎉 Update CommentsSection to use infinite scrolling for comments

--- src/modules/videos/ui/sections/comments-sections.tsx
+++ src/modules/videos/ui/sections/comments-sections.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+import { InfiniteScroll } from "@/components/infinite-scroll";
+import { DEFAULT_LIMIT } from "@/lib/constants";
 import { CommentForm } from "@/modules/comments/ui/components/comment-form";
+import { CommentItem } from "@/modules/comments/ui/components/comment-item";
 import { trpc } from "@/trpc/client";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
@@ -12,15 +15,36 @@ interface CommentsSectionProps {
 const CommentsSectionSuspense: React.FC<CommentsSectionProps> = ({
   videoId,
 }) => {
-  const [comments] = trpc.comments.getMany.useSuspenseQuery({
-    videoId,
-  });
+  const [comments, query] = trpc.comments.getMany.useSuspenseInfiniteQuery(
+    {
+      videoId,
+      limit: DEFAULT_LIMIT,
+    },
+    {
+      getNextPageParam: (lastPage) => lastPage.nextCursor,
+    }
+  );
 
   return (
     <div className="mt-6">
       <div className="flex flex-col gap-6">
-        <h2 className="text-lg font-medium">{comments.length} Comments</h2>
+        <h2 className="text-lg font-bold">
+          {query.data?.pages[0].totalComments} Comments
+        </h2>
         <CommentForm videoId={videoId} />
+
+        <div className="flex flex-col gap-6">
+          {comments.pages
+            .flatMap((page) => page.items)
+            .map((comment) => (
+              <CommentItem key={comment.id} comment={comment} />
+            ))}
+          <InfiniteScroll
+            hasNextPage={query.hasNextPage}
+            ifFetchingNextPage={query.isFetchingNextPage}
+            fetchNextPage={query.fetchNextPage}
+          />
+        </div>
       </div>
     </div>
   );
