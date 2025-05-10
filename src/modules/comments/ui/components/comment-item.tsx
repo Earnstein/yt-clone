cc6deffe993c86ff3f16078cdc5226332fe2589b
earnstein <bdamilola00@gmail.com>
2025-05-10 21:28:54 +0100

    feat: 🎉 Enhance CommentItem with delete functionality and dropdown menu

--- src/modules/comments/ui/components/comment-item.tsx
+++ src/modules/comments/ui/components/comment-item.tsx
@@ -1,15 +1,49 @@
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { UserAvatar } from "@/components/user-avatar";
-import { getFullName } from "@/lib/utils";
+import { cn, getFullName } from "@/lib/utils";
+import { trpc } from "@/trpc/client";
+import { useClerk, useUser } from "@clerk/nextjs";
 import { formatDistanceToNow } from "date-fns";
+import {
+  EllipsisVerticalIcon,
+  MessageSquareIcon,
+  Trash2Icon,
+} from "lucide-react";
 import Link from "next/link";
+import { toast } from "sonner";
 import { TCommentsGetAll } from "../../types";
 interface CommentItemProps {
   comment: TCommentsGetAll["items"][number];
 }
 
 export const CommentItem: React.FC<CommentItemProps> = ({ comment }) => {
+  const { user } = useUser();
+  const utils = trpc.useUtils();
+  const { openSignIn } = useClerk();
+  const { mutate: deleteComment, isPending } = trpc.comments.delete.useMutation(
+    {
+      onSuccess: () => {
+        toast.success("Comment deleted");
+        utils.comments.getMany.invalidate();
+      },
+      onError: (error) => {
+        toast.error("Something went wrong");
+        if (error.data?.code === "UNAUTHORIZED") {
+          toast.error("You are not authorized to delete this comment");
+          openSignIn({ redirectUrl: "/sign-in" });
+        }
+      },
+    }
+  );
+
   return (
-    <div className="flex flex-col gap-2">
+    <div className={cn("flex flex-col gap-2", isPending && "opacity-50")}>
       <div className="flex gap-4">
         <Link href={`/users/${comment.userId}`}>
           <UserAvatar
@@ -30,7 +64,32 @@ export const CommentItem: React.FC<CommentItemProps> = ({ comment }) => {
             </p>
           </Link>
           <p className="text-sm text-muted-foreground">{comment.comment}</p>
+          {/* TODO: Reactions */}
         </div>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" size="icon">
+              <EllipsisVerticalIcon className="size-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem onClick={() => {}}>
+              <MessageSquareIcon className="size-4" />
+              Reply
+            </DropdownMenuItem>
+            {user?.id === comment.userId && (
+              <DropdownMenuItem
+                onClick={() => {
+                  deleteComment({ id: comment.id });
+                }}
+                disabled={isPending}
+              >
+                <Trash2Icon className="size-4" />
+                Delete
+              </DropdownMenuItem>
+            )}
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </div>
   );
