d7cdfb488c44af9d69577731aee14feaed9472e4
earnstein <bdamilola00@gmail.com>
2025-06-16 23:02:27 +0100

    feat: 🎉 Implement suggestions fetching in SuggestionsSection

--- src/modules/videos/ui/sections/suggestions-sections.tsx
+++ src/modules/videos/ui/sections/suggestions-sections.tsx
@@ -1,3 +1,29 @@
-export const SuggestionsSection = () => {
-  return <div>SuggestionsSection</div>;
+"use client";
+
+import { DEFAULT_LIMIT } from "@/lib/constants";
+import { trpc } from "@/trpc/client";
+
+interface SuggestionsSectionProps {
+  videoId: string;
+}
+
+export const SuggestionsSection: React.FC<SuggestionsSectionProps> = ({
+  videoId,
+}) => {
+  const [data] = trpc.suggestions.getSuggestions.useSuspenseInfiniteQuery(
+    { videoId, limit: DEFAULT_LIMIT },
+    { getNextPageParam: (lastPage) => lastPage.nextCursor }
+  );
+  return (
+    <div>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+      {data.pages.map((page) => (
+        <div key={page.nextCursor?.updatedAt.toISOString()}>
+          {page.items.map((item) => (
+            <div key={item.id}>{item.title}</div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 };
