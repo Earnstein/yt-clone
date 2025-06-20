2deccfe9d4aa26ee353834516ad1ea935e9b8a44
earnstein <bdamilola00@gmail.com>
2025-06-20 23:51:18 +0100

    feat: 🎉 Add TGetManyVideosOutput type for video suggestions

--- src/modules/videos/types.ts
+++ src/modules/videos/types.ts
@@ -3,3 +3,6 @@ import { inferRouterOutputs } from "@trpc/server";
 
 export type TGetOneVideoOutput =
   inferRouterOutputs<AppRouter>["videos"]["getOne"];
+
+export type TGetManyVideosOutput =
+  inferRouterOutputs<AppRouter>["suggestions"]["getSuggestions"]; //TODO: To change to videos getmany
