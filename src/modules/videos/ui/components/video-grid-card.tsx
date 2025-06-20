0b5044934ab6fa52b8348f3014ab1309489b8d22
earnstein <bdamilola00@gmail.com>
2025-06-20 23:51:00 +0100

    feat: 🎉 Add VideoGridCard component for video display

---
+++ src/modules/videos/ui/components/video-grid-card.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import { TGetManyVideosOutput } from "../../types";
+import { VideoInfo } from "./video-info";
+import { VideoThumbnail } from "./video-thumbnail";
+
+interface VideoGridCardProps {
+  video: TGetManyVideosOutput["items"][number];
+  onRemove?: () => void;
+}
+
+export const VideoGridCard: React.FC<VideoGridCardProps> = ({
+  video,
+  onRemove,
+}) => {
+  return (
+    <div className="flex flex-col gap-2 w-full group">
+      <Link href={`/videos/${video.id}`}>
+        <VideoThumbnail
+          thumbnailUrl={video.thumbnailUrl}
+          title={video.title}
+          duration={video.duration}
+          previewUrl={video.previewUrl}
+        />
+      </Link>
+
+      <VideoInfo video={video} onRemove={onRemove} />
+    </div>
+  );
+};
