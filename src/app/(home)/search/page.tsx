053dab6b25baa01cdbcd5f77a31308de3b5e2033
earnstein <bdamilola00@gmail.com>
2025-06-22 18:33:50 +0100

    feat: 🎉 Create search page component with dynamic rendering

---
+++ src/app/(home)/search/page.tsx
@@ -0,0 +1,24 @@
+import { SearchView } from "@/modules/search/ui/view/search-view";
+import { HydrateClient, trpc } from "@/trpc/server";
+import { NextPage } from "next";
+
+export const dynamic = "force-dynamic";
+
+interface PageProps {
+  searchParams: Promise<{
+    query: string;
+    categoryId: string | undefined;
+  }>;
+}
+
+const page: NextPage<PageProps> = async ({ searchParams }) => {
+  const { query, categoryId } = await searchParams;
+  void trpc.categories.getAll.prefetch();
+  return (
+    <HydrateClient>
+      <SearchView query={query} categoryId={categoryId} />
+    </HydrateClient>
+  );
+};
+
+export default page;
