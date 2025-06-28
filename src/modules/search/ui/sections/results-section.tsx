091c3d567541aa6b71f22518f161391649119454
earnstein <bdamilola00@gmail.com>
2025-06-28 22:40:57 +0100

    fix: 🔧 Add skeleton loading state to results section

--- src/modules/search/ui/sections/results-section.tsx
+++ src/modules/search/ui/sections/results-section.tsx
@@ -2,17 +2,41 @@
 
 import { InfiniteScroll } from "@/components/infinite-scroll";
 import { useIsMobile } from "@/hooks/use-mobile";
-import { VideoGridCard } from "@/modules/videos/ui/components/video-grid-card";
-import { VideoRowCard } from "@/modules/videos/ui/components/video-row-card";
+import {
+  VideoGridCard,
+  VideoGridCardSkeleton,
+} from "@/modules/videos/ui/components/video-grid-card";
+import {
+  VideoRowCard,
+  VideoRowCardSkeleton,
+} from "@/modules/videos/ui/components/video-row-card";
 import { trpc } from "@/trpc/client";
-import { Fragment } from "react";
+import { Fragment, Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 interface ResultsSectionProps {
   query: string | undefined;
   categoryId: string | undefined;
 }
 
-export const ResultsSection = ({ query, categoryId }: ResultsSectionProps) => {
+const ResultsSectionSkeleton = () => {
+  return (
+    <div>
+      <div className="hidden flex-col gap-4 md:flex">
+        {Array.from({ length: 5 }).map((_, index) => (
+          <VideoRowCardSkeleton key={index} />
+        ))}
+      </div>
+      <div className="flex flex-col gap-y-10 gap-x-4">
+        {Array.from({ length: 5 }).map((_, index) => (
+          <VideoGridCardSkeleton key={index} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const ResultsSectionSuspense = ({ query, categoryId }: ResultsSectionProps) => {
   const isMobile = useIsMobile();
   const [results, resultsQuery] =
     trpc.search.getSearchResults.useSuspenseInfiniteQuery(
@@ -46,5 +70,18 @@ export const ResultsSection = ({ query, categoryId }: ResultsSectionProps) => {
         fetchNextPage={resultsQuery.fetchNextPage}
       />
     </Fragment>
+  );
+};
+
+export const ResultsSection = ({ query, categoryId }: ResultsSectionProps) => {
+  return (
+    <Suspense
+      fallback={<ResultsSectionSkeleton />}
+      key={`${query}-${categoryId}`}
+    >
+      <ErrorBoundary fallback={<div>Error</div>}>
+        <ResultsSectionSuspense query={query} categoryId={categoryId} />
+      </ErrorBoundary>
+    </Suspense>
   );
 };
