494009113b06ba13f6d9388a321c649d0f79bb9d
earnstein <bdamilola00@gmail.com>
2025-06-28 17:01:37 +0100

    feat: 🎉 Add search router to appRouter

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,6 +1,7 @@
 import { categoriesRouter } from "@/modules/categrories/server/procedure";
 import { commentReactionsRouter } from "@/modules/comment-reactions/server/procedure";
 import { commentsRouter } from "@/modules/comments/server/procedures";
+import { searchRouter } from "@/modules/search/server/procedure";
 import { studioRouter } from "@/modules/studio/server/procedures";
 import { subscriptionsRouter } from "@/modules/subscriptions/server/procedure";
 import { suggestionsRouter } from "@/modules/suggestions/server/procedure";
@@ -18,6 +19,7 @@ export const appRouter = createTRPCRouter({
   comments: commentsRouter,
   commentReactions: commentReactionsRouter,
   suggestions: suggestionsRouter,
+  search: searchRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
