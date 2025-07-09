04a76a07f7f244fd45f9a5ed1e2b2cbdce49bcfb
earnstein <bdamilola00@gmail.com>
2025-07-09 19:50:44 +0100

    feat: 🎥 Implement getHomeVideos query for fetching videos

---
+++ src/modules/home/server/procedure.ts
@@ -0,0 +1,81 @@
+import { db } from "@/db";
+import { users, videoReactions, videos, videoViews } from "@/db/schema";
+import { DEFAULT_LIMIT } from "@/lib/constants";
+import { baseProcedure, createTRPCRouter } from "@/trpc/init";
+import { and, desc, eq, getTableColumns, lt, or } from "drizzle-orm";
+import { z } from "zod";
+
+export const homeRouter = createTRPCRouter({
+  getHomeVideos: baseProcedure
+    .input(
+      z.object({
+        categoryId: z.string().nullish(),
+        cursor: z
+          .object({
+            id: z.string(),
+            updatedAt: z.date(),
+          })
+          .nullish(),
+        limit: z.number().min(1).max(100).default(DEFAULT_LIMIT),
+      })
+    )
+    .query(async ({ input }) => {
+      const { cursor, limit, categoryId } = input;
+
+      const data = await db
+        .select({
+          ...getTableColumns(videos),
+          user: users,
+          viewCount: db.$count(videoViews, eq(videoViews.videoId, videos.id)),
+          likeCount: db.$count(
+            videoReactions,
+            and(
+              eq(videoReactions.videoId, videos.id),
+              eq(videoReactions.type, "like")
+            )
+          ),
+          dislikeCount: db.$count(
+            videoReactions,
+            and(
+              eq(videoReactions.videoId, videos.id),
+              eq(videoReactions.type, "dislike")
+            )
+          ),
+        })
+        .from(videos)
+        .innerJoin(users, eq(videos.userId, users.id))
+        .where(
+          and(
+            eq(videos.visibility, "public"),
+            categoryId ? eq(videos.categoryId, categoryId) : undefined,
+            cursor
+              ? or(
+                  lt(videos.updatedAt, cursor.updatedAt),
+                  and(
+                    eq(videos.updatedAt, cursor.updatedAt),
+                    lt(videos.id, cursor.id)
+                  )
+                )
+              : undefined
+          )
+        )
+        .orderBy(desc(videos.updatedAt), desc(videos.id))
+        .limit(limit + 1);
+
+      const hasMore = data.length > limit;
+      const items = hasMore ? data.slice(0, -1) : data;
+
+      const lastItem = items[items.length - 1];
+      const nextCursor = hasMore
+        ? {
+            id: lastItem.id,
+            updatedAt: lastItem.updatedAt,
+          }
+        : null;
+
+      return {
+        items,
+        nextCursor,
+      };
+    }),
+});
