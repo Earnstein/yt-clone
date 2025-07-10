29a374a76074e62f86688198425893d89d00a700
earnstein <bdamilola00@gmail.com>
2025-07-10 10:30:32 +0100

    feat: 🎥 Implement getTrendingVideos query for fetching trending videos

--- src/modules/home/server/procedure.ts
+++ src/modules/home/server/procedure.ts
@@ -78,4 +78,82 @@ export const homeRouter = createTRPCRouter({
         nextCursor,
       };
     }),
+  getTrendingVideos: baseProcedure
+    .input(
+      z.object({
+        cursor: z
+          .object({
+            id: z.string(),
+            viewCount: z.number(),
+          })
+          .nullish(),
+        limit: z.number().min(1).max(100).default(DEFAULT_LIMIT),
+      })
+    )
+    .query(async ({ input }) => {
+      const { cursor, limit } = input;
+
+      const viewCountSubquery = db.$count(
+        videoViews,
+        eq(videoViews.videoId, videos.id)
+      );
+      const likeCountSubquery = db.$count(
+        videoReactions,
+        and(
+          eq(videoReactions.videoId, videos.id),
+          eq(videoReactions.type, "like")
+        )
+      );
+
+      const dislikeCountSubquery = db.$count(
+        videoReactions,
+        and(
+          eq(videoReactions.videoId, videos.id),
+          eq(videoReactions.type, "dislike")
+        )
+      );
+
+      const data = await db
+        .select({
+          ...getTableColumns(videos),
+          user: users,
+          viewCount: viewCountSubquery,
+          likeCount: likeCountSubquery,
+          dislikeCount: dislikeCountSubquery,
+        })
+        .from(videos)
+        .innerJoin(users, eq(videos.userId, users.id))
+        .where(
+          and(
+            eq(videos.visibility, "public"),
+            cursor
+              ? or(
+                  lt(viewCountSubquery, cursor.viewCount),
+                  and(
+                    eq(viewCountSubquery, cursor.viewCount),
+                    lt(videos.id, cursor.id)
+                  )
+                )
+              : undefined
+          )
+        )
+        .orderBy(desc(viewCountSubquery), desc(videos.id))
+        .limit(limit + 1);
+
+      const hasMore = data.length > limit;
+      const items = hasMore ? data.slice(0, -1) : data;
+
+      const lastItem = items[items.length - 1];
+      const nextCursor = hasMore
+        ? {
+            id: lastItem.id,
+            viewCount: lastItem.viewCount,
+          }
+        : null;
+
+      return {
+        items,
+        nextCursor,
+      };
+    }),
 });
