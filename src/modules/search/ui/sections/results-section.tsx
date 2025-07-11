f00a140660d528783b2116eac2155a5204d35746
earnstein <bdamilola00@gmail.com>
2025-07-11 08:42:11 +0100

    feat: 🎉 Refactor ResultsSectionSuspense for responsive video display

--- src/modules/search/ui/sections/results-section.tsx
+++ src/modules/search/ui/sections/results-section.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { InfiniteScroll } from "@/components/infinite-scroll";
-import { useIsMobile } from "@/hooks/use-mobile";
 import {
   VideoGridCard,
   VideoGridCardSkeleton,
@@ -37,7 +36,6 @@ const ResultsSectionSkeleton = () => {
 };
 
 const ResultsSectionSuspense = ({ query, categoryId }: ResultsSectionProps) => {
-  const isMobile = useIsMobile();
   const [results, resultsQuery] =
     trpc.search.getSearchResults.useSuspenseInfiniteQuery(
       {
@@ -50,20 +48,16 @@ const ResultsSectionSuspense = ({ query, categoryId }: ResultsSectionProps) => {
   const videos = results.pages.flatMap((page) => page.items);
   return (
     <Fragment>
-      {isMobile ? (
-        <div className="flex flex-col gap-y-10 gap-x-4">
-          {videos.map((video) => (
-            <VideoGridCard key={video.id} video={video} />
-          ))}
-        </div>
-      ) : (
-        <div className="flex flex-col gap-4">
-          {videos.map((video) => (
-            <VideoRowCard key={video.id} video={video} />
-          ))}
-        </div>
-      )}
-
+      <div className="flex flex-col gap-y-10 gap-x-4 md:hidden">
+        {videos.map((video) => (
+          <VideoGridCard key={video.id} video={video} />
+        ))}
+      </div>
+      <div className="hidden flex-col gap-4 md:flex">
+        {videos.map((video) => (
+          <VideoRowCard key={video.id} video={video} />
+        ))}
+      </div>
       <InfiniteScroll
         hasNextPage={resultsQuery.hasNextPage}
         ifFetchingNextPage={resultsQuery.isFetchingNextPage}
