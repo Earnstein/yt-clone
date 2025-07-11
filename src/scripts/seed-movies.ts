df31839e135096abebb5267d1fd739041ad2e3be
earnstein <bdamilola00@gmail.com>
2025-07-11 21:22:46 +0100

    feat: 🎉 Generate random video data for seeding

--- src/scripts/seed-movies.ts
+++ src/scripts/seed-movies.ts
@@ -2,81 +2,28 @@ import { db } from "@/db";
 import { categories, users, videos } from "@/db/schema";
 import { VIDEO_ID_PREFIX } from "@/lib/constants";
 import { generateUniqueId } from "@/lib/utils";
+import { faker } from "@faker-js/faker";
+
+const createRandomVideo = () => {
+  return {
+    title: faker.word.words(3),
+    description: faker.lorem.paragraph(1),
+    visibility: faker.helpers.arrayElement(["public", "private"]),
+    createdAt: faker.date.past(),
+    updatedAt: faker.date.recent(),
+  };
+};
 type Video = {
   title: string;
   description: string;
   visibility: "public" | "private";
+  createdAt: Date;
+  updatedAt: Date;
 };
-const videosData: Video[] = [
-  {
-    title: "The Matrix",
-    description:
-      "A computer hacker learns about the true nature of his reality and his role in the war against its controllers.",
-    visibility: "public",
-  },
-  {
-    title: "The Matrix Reloaded",
-    description:
-      "The Matrix Reloaded is a 2003 American science fiction action film and the second installment in The Matrix film series.",
-    visibility: "public",
-  },
-  {
-    title: "The Matrix Revolutions",
-    description:
-      "The Matrix Revolutions is a 2003 American science fiction action film and the third installment in The Matrix film series.",
-    visibility: "public",
-  },
-  {
-    title: "The Matrix Resurrections",
-    description:
-      "The Matrix Resurrections is a 2021 American science fiction action film and the fourth installment in The Matrix film series.",
-    visibility: "public",
-  },
-  {
-    title: "The Avengers",
-    description:
-      "The Avengers is a 2012 American science fiction action film and the first installment in The Avengers film series.",
-    visibility: "public",
-  },
-  {
-    title: "The Avengers: Age of Ultron",
-    description:
-      "The Avengers: Age of Ultron is a 2015 American science fiction action film and the second installment in The Avengers film series.",
-    visibility: "public",
-  },
-  {
-    title: "Agatha Christie's And Then There Were None",
-    description:
-      "Agatha Christie's And Then There Were None is a 2015 American science fiction action film and the third installment in The Avengers film series.",
-    visibility: "public",
-  },
-  {
-    title: "Harry Potter and the Philosopher's Stone",
-    description:
-      "Harry Potter and the Philosopher's Stone is a 2001 American science fiction action film and the first installment in The Harry Potter film series.",
-    visibility: "public",
-  },
-  {
-    title: "Harry Potter and the Chamber of Secrets",
-    description:
-      "Harry Potter and the Chamber of Secrets is a 2002 American science fiction action film and the second installment in The Harry Potter film series.",
-    visibility: "public",
-  },
-  {
-    title: "Harry Potter and the Prisoner of Azkaban",
-    description:
-      "Harry Potter and the Prisoner of Azkaban is a 2004 American science fiction action film and the third installment in The Harry Potter film series.",
-    visibility: "public",
-  },
-];
+const videosData: Video[] = Array.from({ length: 100 }, createRandomVideo);
 
 async function seedMovies() {
   try {
-    const existingVideos = await db.select().from(videos);
-    if (existingVideos.length > 0) {
-      console.log("🛑 Videos already seeded");
-      process.exit(0);
-    }
     const [allCategories, allUsers] = await Promise.all([
       db.select().from(categories),
       db.select().from(users),
@@ -94,6 +41,8 @@ async function seedMovies() {
         visibility: video.visibility as "public" | "private",
         title: video.title,
         description: video.description,
+        createdAt: video.createdAt,
+        updatedAt: video.updatedAt,
       }))
     );
     console.log("🌱 Videos seeded successfully");
