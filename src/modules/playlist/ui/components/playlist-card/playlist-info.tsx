9b5f100c7f2d3b58c63add80d7f6ed87285e7f1b
earnstein <bdamilola00@gmail.com>
2025-07-17 22:58:59 +0100

    feat: 🎉 Add dropdown menu for editing and deleting playlists in PlaylistInfo component

--- src/modules/playlist/ui/components/playlist-card/playlist-info.tsx
+++ src/modules/playlist/ui/components/playlist-card/playlist-info.tsx
@@ -1,5 +1,18 @@
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { Skeleton } from "@/components/ui/skeleton";
 import { TPlaylists } from "@/modules/playlist/types";
+import { EditPlaylistModal } from "@/modules/videos/ui/components/edit-playlist-modal";
+import { trpc } from "@/trpc/client";
+import { EllipsisVerticalIcon, PencilIcon, Trash2Icon } from "lucide-react";
+import { Fragment, useState } from "react";
+import { toast } from "sonner";
 
 interface PlaylistInfoProps {
   playlist: TPlaylists[number];
@@ -18,17 +31,57 @@ export const PlaylistInfoSkeleton = () => {
 };
 
 export const PlaylistInfo: React.FC<PlaylistInfoProps> = ({ playlist }) => {
+  const [open, setOpen] = useState(false);
+  const utils = trpc.useUtils();
+
+  const deletePlaylist = trpc.playlist.deletePlaylist.useMutation({
+    onSuccess: ({ deletedPlaylist }) => {
+      toast.success(`${deletedPlaylist.name} playlist deleted successfully`);
+      utils.playlist.getPlaylists.invalidate();
+    },
+    onError: ({ message }) => {
+      toast.error(message || "Something went wrong");
+    },
+  });
+
+  const handleDeletePlaylist = () => {
+    return deletePlaylist.mutate({
+      playlistId: playlist.id,
+    });
+  };
+
   return (
-    <div className="flex gap-3">
-      <div className="flex-1 min-w-0">
-        <h3 className="text-sm font-medium line-clamp-1 lg:line-clamp-2 break-words">
-          {playlist.name}
-        </h3>
-        <p className="text-xs text-muted-foreground">Playlist</p>
-        <p className="text-xs text-muted-foreground group-hover:text-primary">
-          View full playlist
-        </p>
+    <Fragment>
+      <div className="flex gap-3">
+        <div className="flex-1 min-w-0">
+          <h3 className="text-sm font-medium line-clamp-1 lg:line-clamp-2 break-words">
+            {playlist.name}
+          </h3>
+          <p className="text-xs text-muted-foreground">Playlist</p>
+          <p className="text-xs text-muted-foreground group-hover:text-primary">
+            View full playlist
+          </p>
+        </div>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" size="icon" className="rounded-full">
+              <EllipsisVerticalIcon />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => setOpen(true)}>
+              <PencilIcon className="mr-2 size-4" />
+              Edit
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem onClick={handleDeletePlaylist}>
+              <Trash2Icon className="mr-2 size-4" />
+              Delete
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
-    </div>
+      <EditPlaylistModal open={open} setOpen={setOpen} playlist={playlist} />
+    </Fragment>
   );
 };
