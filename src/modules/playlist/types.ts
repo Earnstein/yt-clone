b9bb57758d6ac21680914c7175fe7dc9f208aff8
earnstein <bdamilola00@gmail.com>
2025-07-18 21:50:41 +0100

    feat: 🎉 Add TGetOnePlaylistOutput type for playlist video output

--- src/modules/playlist/types.ts
+++ src/modules/playlist/types.ts
@@ -3,3 +3,6 @@ import { inferRouterOutputs } from "@trpc/server";
 
 export type TPlaylists =
   inferRouterOutputs<AppRouter>["playlist"]["getPlaylists"]["items"];
+
+export type TGetOnePlaylistOutput =
+  inferRouterOutputs<AppRouter>["playlist"]["getPlaylistVideos"]["items"];
